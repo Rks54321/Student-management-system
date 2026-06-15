f5c289cf7972e14e47a8cc861438d404c1db0233
Rks54321 <rakeshkumar756131@gmail.com>
2026-06-15 16:55:19 +0530

    Initial Commit

---
+++ student_management_system/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student_management_system',
+        'USER': 'student_management_system',
+        'PASSWORD': 'student_management_password',
+        'HOST':'localhost',
+        'PORT':'3306'
